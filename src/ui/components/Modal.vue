6e56a547416cd1ccced713cecf08d03959ff8fe6
Arantza <arantza.beitia@gmail.com>
2021-04-23 10:38:46 +0200

    movie detail page UI

--- src/ui/components/Modal.vue
+++ src/ui/components/Modal.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
-    <b-button v-b-modal.modal-1>Launch demo modal</b-button>
+    <b-button v-b-modal.modal-1>
+      <span>Launch demo modal</span>
+      <font-awesome-icon icon="home"/>
+    </b-button>
 
     <b-modal id="modal-1" title="BootstrapVue">
       <p class="my-4">Hello from modal!</p>
--- src/ui/views/movie/Movie.vue
+++ src/ui/views/movie/Movie.vue
@@ -1,12 +1,28 @@
 <template>
   <div class="movie-detail">
-    <div class="movie-detail__inner">
+    <div class="movie-detail__inner pt-5">
       <b-container>
         <section class="row">
-          <article class="col-6">
-            <font-awesome-icon icon="home"/>
-            <font-awesome-icon icon="building"/>
-            <Modal />
+          <article class="col-5">
+            <img 
+              src="https://image.tmdb.org/t/p/original/3tZwNIVrax1IV5BkGzWTYMmjYI5.jpg" 
+              alt="Mortal Kombat" 
+              class="img-fluid movie-detail__image"
+            />
+          </article>
+          <article class="col-7">
+            <header class="movie-detail__header">
+              <h1>Mortal Kombat</h1>
+              <span>2021</span>
+              <Modal />
+            </header>
+            <h2>General</h2>
+            <p>Overview</p>
+            <h2>Generos</h2>
+            <ul>
+              <li>generos 1</li>
+              <li>generos 2</li>
+            </ul>
           </article>
         </section>
       </b-container>
@@ -32,10 +48,21 @@ export default {
   background-size: cover;
   background-color: #464646;
   height: calc(100vh - 60px);
+  color: white;
 
   &__inner {
     height: 100%;
     background-color: rgba(0, 0, 0, 0.8);
+  }
+
+  &__image {
+    border: 2px solid white;
+  }
+
+  &__header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
   }
 }
 </style>
